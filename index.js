5eeb9603b6acfbb0a0e603eb2b9004879739427c
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-07-22 22:40:23 -0700

    Add admin user routes to API for managing admin users

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ app.use('/api', mainRoutes);
 const blogPostRoutes = require('./routes/blogPostRoutes');
 app.use('/api/blog-posts', blogPostRoutes);
 
+const adminUserRoutes = require('./routes/adminUserRoutes');
+app.use('/api/admin-users', adminUserRoutes);
+
 // Start server
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
