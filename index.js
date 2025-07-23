c23cbacb2b66a8aecec8391f8b96e44daeab2526
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-07-23 03:50:44 -0700

    Add CORS middleware to allow requests from specified origins

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://advacned-tsp.onrender.com/']
+}));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
