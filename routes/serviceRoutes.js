f0a0793e05c85152b615b8557ab64f8919bd792a
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-07-29 05:30:06 -0700

    update the route to use slug

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -4,6 +4,7 @@ const auth = require('../middleware/auth');
 const {
   getAllServices,
   getServiceById,
+  getServiceWithSubService,
   createService,
   updateService,
   deleteService,
@@ -12,15 +13,19 @@ const {
   deleteSubService
 } = require('../controller/serviceController');
 
-// Public
+// Public routes
 router.get('/', getAllServices);
-router.get('/:id', getServiceById);
+router.get('/:slug/sub/:subSlug', getServiceWithSubService); // sub-service route
+router.get('/:slug', getServiceById); // id or slug route
+router.get('/:slug/:subSlug', getServiceWithSubService); // two segments, both required
+router.get('/:slug', getServiceById); // one segment, could be id or slug
 
-// Protected
+// Protected routes
 router.post('/', auth, createService);
 router.put('/:id', auth, updateService);
 router.delete('/:id', auth, deleteService);
 
+// Sub-service routes
 router.post('/:id/subservice', auth, addSubService);
 router.put('/:id/subservice/:subSlug', auth, updateSubService);
 router.delete('/:id/subservice/:subSlug', auth, deleteSubService);
